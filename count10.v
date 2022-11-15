21c812c168fa371a90867f959dff9d20fb48ddbb
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2022-11-15 17:35:52 -0500

    Update count10.v

--- count10.v
+++ count10.v
@@ -4,7 +4,7 @@
 'include "Clk_divider.v"
 
 //Jaleel Created
-module count10(clock,inc,reset, count, count_eq_9)
+module count10(clock,inc,reset, Count, count_eq_9)
 
 input clock, inc, reset;
 output [3:0] Count;
@@ -23,4 +23,25 @@ RCA comp2 ( count, inc, RCA_out);
 assign count_eq_9= (count== 4'b1001) ? 1: 0;
 assign rst= (count_eq_9 & inc) | reset;
 
+wire [19:0] din;
+wire [19:0] clkdiv;
+ 
+DFF0 dff_inst0(
+    .data_in(din[0]),
+	 .clock(clock),
+	 .reset(rst),
+    .data_out(clkdiv[0])
+);
+
+genvar i;
+generate
+for (i = 1; i < 19; i=i+1) 
+	begin : dff_gen_label
+		 DFF0 dff_inst (
+			  .data_in (din[i]),
+			  .clock(clkdiv[i-1]),
+			  .reset(rst),
+			  .data_out(clkdiv[i])
+		 );
+endgenerate
 endmodule
